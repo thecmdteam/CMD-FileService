e788473c5c49001e5096db2fc04dbbd5b6b15c26
EbubeCode <chukwuma258@gmail.com>
2021-10-11 13:36:35 +0100

    make it better

---
+++ src/main/java/com/cmd/cmdfileservice/CmdFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cmd.cmdfileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmdFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmdFileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cmd/cmdfileservice/CmdFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cmd.cmdfileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CmdFileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
